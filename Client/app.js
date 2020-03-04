ad2fc3173fbba57948bf915e8a25d7885268c56a
Anthony Gomez <gomeztony004@gmail.com>
2020-03-04 12:06:26 -0600

    refactored serverside to make sure the api is working properly in all methods

--- Client/app.js
+++ Client/app.js
@@ -6,20 +6,7 @@
             Genre: this["Genre"].value
         };
 
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'get',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre movieTable').html( data );
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-            
-        });
+        
 
         $.ajax({
             url: 'https://localhost:44325/api/movie',
@@ -35,27 +22,46 @@
             }
         });
 
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'put',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            succes: function(data, textStatus, jQxhr ){
-                $('#navbar-nav mr-auto').find("#title").html(data);
-            },
-            error: function(jQxhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
-
         
 
+        
         e.preventDefault();
-        
     }
-   
-        
 
     $('#my-form').submit( processForm );
 })(jQuery);
+
+function searchLibrary($){
+    $.ajax({
+        url: 'https://localhost:44325/api/movie',
+        dataType: 'json',
+        type: 'put',
+        contentType: 'application/json',
+        data: JSON.stringify(dict),
+        succes: function(data, textStatus, jQxhr ){
+            $('#navbar-nav mr-auto').find("#title").html(data);
+        },
+        error: function(){
+            // $('.playerData').html('';) clears data for table so it doesn't duplicate it
+            console.log( errorThrown );
+        }
+    }).then 
+};
+
+$.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'get',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre movieTable').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+            
+        });
+
+
+
